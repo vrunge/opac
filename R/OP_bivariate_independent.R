7ddfcaed9e5b746a1b95017b64697838e2a0ad80
Vincent Runge <vrunge@tora.maths.univ-evry.fr>
2022-08-18 16:10:26 +0200

    OP1C algo first working version

--- R/OP_bivariate_independent.R
+++ R/OP_bivariate_independent.R
@@ -12,7 +12,7 @@
 #' @return a list with the change-point elements (each last index of each segment)
 #' @examples
 #' OP_2D(dataGenerator2D(chpts = c(30,100,120), means1 = c(0,5,0), means2 = c(7,1,-4)))
-OP_2D <- function(data, beta = 4*log(length(data$y1)))
+OP_2D <- function(data, beta = 4 * log(length(data$y1)))
 {
   ###
   ### INITIALIZATION
@@ -30,24 +30,24 @@ OP_2D <- function(data, beta = 4*log(length(data$y1)))
   index <- 0 #best index (to be found) for last change-point
 
   ###
-  ### UPDATE rule Dynamic programming
+  ### UPDATE rule Dynamic Programming
   ###
   for(t in 1:n)
   {
     min_temp <- Inf
     for(k in 1:t)
     {
-      eval <- cumyS[t+1]-cumyS[k] - ((cumy1[t+1]-cumy1[k])^2+(cumy2[t+1]-cumy2[k])^2)/(t-k+1) + costQ[k] + beta
+      eval <- cumyS[t+1] - cumyS[k] - ((cumy1[t+1] - cumy1[k])^2 + (cumy2[t+1] - cumy2[k])^2)/(t-k+1) + costQ[k] + beta
       if(eval < min_temp){min_temp <- eval; index <- k}
     }
     costQ[t+1] <- min_temp
     cp[t+1] <- index - 1
   }
 
-  cp <- cp[-1] # remove first value
   ###
   ### backtracking step
   ###
+  cp <- cp[-1] # remove first value
   changepoints <- n # vector of change-point to build
   current <- n
 
@@ -73,7 +73,7 @@ OP_2D <- function(data, beta = 4*log(length(data$y1)))
 #' @return a list with the change-point elements (each last index of each segment) and a vector nb counting the number of non-pruned elements at each iteration
 #' @examples
 #' OP_2D_PELT(dataGenerator2D(chpts = c(30,100,120), means1 = c(0,1,0), means2 = c(7,1,-4)))
-OP_2D_PELT <- function(data, beta = 4*log(length(data$y1)))
+OP_2D_PELT <- function(data, beta = 4 * log(length(data$y1)))
 {
   ###
   ### INITIALIZATION
@@ -88,19 +88,18 @@ OP_2D_PELT <- function(data, beta = 4*log(length(data$y1)))
   cp <- rep(0, n + 1) #cp vector cp[k] = index of the last change-point for data y(1) to y(k-1)
   costQ <- rep(0, n + 1) # costQ[k] optimal cost for data y(1) to y(k-1)
   costQ[1] <- -beta
-  index <- 0 #best index (to be found) for last change-point
 
   nb <- rep(0, n) #nb element for minimization in DP
   indexSet <- 1
   ###
-  ### update rule Dynamic programming
+  ### update rule Dynamic Programming
   ###
   for(t in 1:n)
   {
     min_temp <- Inf
     for(k in indexSet)
     {
-      eval <- cumyS[t+1]-cumyS[k] - ((cumy1[t+1]-cumy1[k])^2+(cumy2[t+1]-cumy2[k])^2)/(t-k+1) + costQ[k] + beta
+      eval <- cumyS[t+1] - cumyS[k] - ((cumy1[t+1] - cumy1[k])^2 + (cumy2[t+1] - cumy2[k])^2)/(t-k+1) + costQ[k] + beta
       if(eval < min_temp){min_temp <- eval; index <- k}
     }
     costQ[t+1] <- min_temp
@@ -111,7 +110,7 @@ OP_2D_PELT <- function(data, beta = 4*log(length(data$y1)))
     for(k in indexSet)
     {
       ### PRUNING STEP: reduce indexSet using the pruning test (inquality-based)
-      if(costQ[t+1] > costQ[k] + cumyS[t+1]-cumyS[k] - ((cumy1[t+1]-cumy1[k])^2+(cumy2[t+1]-cumy2[k])^2)/(t-k+1))
+      if(costQ[t+1] > costQ[k] + cumyS[t+1] - cumyS[k] - ((cumy1[t+1] - cumy1[k])^2 + (cumy2[t+1] - cumy2[k])^2)/(t-k+1))
       {
         nonpruned <- c(nonpruned, k)
       }
@@ -120,11 +119,10 @@ OP_2D_PELT <- function(data, beta = 4*log(length(data$y1)))
     nb[t] <- length(indexSet) ### count number of elements
   }
 
-  cp <- cp[-1] # remove first value
-
   ###
   ### backtracking step
   ###
+  cp <- cp[-1] # remove first value
   changepoints <- n # vector of change-point to build
   current <- n
 
@@ -151,9 +149,180 @@ OP_2D_PELT <- function(data, beta = 4*log(length(data$y1)))
 #' @return a list with the change-point elements (each last index of each segment) and a vector nb counting the number of non-pruned elements at each iteration
 #' @examples
 #' OP_2D_1C(dataGenerator2D(chpts = c(30,100,120), means1 = c(0,1,0), means2 = c(7,1,-4)))
-OP_2D_1C <- function(data, beta = 4*log(length(data$y1)))
+OP_2D_1C <- function(data, beta = 4 * log(length(data$y1)))
 {
 
+  ###
+  ### INNER FUNCTIONS
+  ###
+  eval_var <- function(j, k)
+  {
+    if(j + 1 == k){return(0)}
+    len <- k - j
+    eval <- (cumyS[k] - cumyS[j])/len - ((cumy1[k] - cumy1[j])^2 + (cumy2[k] - cumy2[j])^2)/(len^2)
+    return(eval)
+  }
+
+  eval_q_min <- function(k, t)
+  {
+    eval <- cumyS[t] - cumyS[k] - ((cumy1[t] - cumy1[k])^2 + (cumy2[t] - cumy2[k])^2)/(t-k) + costQ[k] + beta
+    return(eval)
+  }
+
+  eval_q <- function(k, t, t1, t2)
+  {
+    y1 <- cumy1[t]-cumy1[k]
+    y2 <- cumy2[t]-cumy2[k]
+    len <- t - k
+    eval <- len*((t1 - (y1/len))^2 +  (t2 - (y2/len))^2) + cumyS[t] - cumyS[k] - ((y1)^2 + (y2)^2)/len + costQ[k] + beta
+    return(eval)
+  }
+
+  ###
+  ### INITIALIZATION
+  ###
+  y1 <- data$y1
+  y2 <- data$y2
+  n <- length(y1)
+  cumy1 <- cumsum(c(0, y1))
+  cumy2 <- cumsum(c(0, y2))
+  cumyS <- cumsum(c(0, y1^2 + y2^2))
+
+  cp <- rep(0, n) #cp vector cp[i] = index of the last change-point for data y(1) to y(i)
+  costQ <- rep(0, n + 1) # costQ[i] optimal cost for data y(1) to y(i-1)
+  costQ[1] <- -beta
+
+  nb <- rep(0, n) #nb element for minimization in DP
+  nrows <- rep(0, n) #nb rows in info dataframe
+
+  info <- data.frame(matrix(ncol = 3, nrow = 0)) ### info for pruning
+  colnames(info) <- c("k", "j", "m")
+  info[1,] <-  c(1, 1, 0)
+  indexSet <- 1 #start with q1
+
+  ###
+  ### update rule Dynamic Programming
+  ###
+  for(t in 2:n)
+  {
+    ######
+    ###### STEP pruning info by costQ[t] + beta = m_{t-1} + beta (costQ[1] = m_0 = -beta)
+    ######
+    ######### 1 ######### find omega_t^k
+    omega_t_k <- rep(-Inf, length(indexSet))
+
+    for(i in 1:nrow(info))
+    {
+      k <- info$k[i]
+      j <- info$j[i]
+      ind <- which(indexSet == k)
+      if(j == k){omega_t_k[ind] <- max(omega_t_k[ind], info$m[i])}
+      else
+      {
+        #to do later, replace by only the test k > j
+        t1k <- (cumy1[t]-cumy1[k])/(t-k)
+        t2k <- (cumy2[t]-cumy2[k])/(t-k)
+        ind <- which(indexSet == k)
+        if(eval_q(k, t, t1k, t2k) > eval_q(j, t, t1k, t2k)){omega_t_k[ind] <- max(omega_t_k[ind], info$m[i])}
+        t1j <- (cumy1[t]-cumy1[j])/(t-j)
+        t2j <- (cumy2[t]-cumy2[j])/(t-j)
+        ind <- which(indexSet == j)
+        if(eval_q(j, t, t1j, t2j) > eval_q(k, t, t1j, t2j)){omega_t_k[ind] <- max(omega_t_k[ind], info$m[i])}
+      }
+    }
+    ######### 2 ######### update indexSet (removing pruned indices)
+    nonpruned <- NULL
+    #print(omega_t_k)
+    #print(costQ)
+    #print(indexSet)
+    #print(info)
+    for(k in 1:length(indexSet))
+    {
+      if(omega_t_k[k] <= costQ[t] + beta){nonpruned <- c(nonpruned, indexSet[k])}
+    }
+    indexSet <- nonpruned
+
+    ######### 3 ######### update info (removing rows with pruned indices)
+    info <- info[(info$k %in% indexSet) & (info$j %in% indexSet), ]
+
+    ###
+    ### STEP updating info with new data point y(t+1)
+    ###
+    ######### 1 ######### updating 3-point already in info
+    for(i in 1:nrow(info)) # update m
+    {
+      k <- info$k[i]
+      j <- info$j[i]
+      if(k == j)
+      {
+        info$m[i] <- eval_q_min(k, t+1)
+      }
+      else
+      {
+        if(j < k){myvar <- eval_var(j,k)}else{{myvar <- eval_var(k,j)}}
+        R <- (costQ[k]-costQ[j])/(k-j) - myvar
+
+        if(j < k)
+        {
+          t1jt <- (cumy1[t+1]-cumy1[j])/(t-j+1)
+          t2jt <- (cumy2[t+1]-cumy2[j])/(t-j+1)
+          t1jk <- (cumy1[k]-cumy1[j])/(k-j)
+          t2jk <- (cumy2[k]-cumy2[j])/(k-j)
+          D <- (t1jt - t1jk)^2 + (t2jt - t2jk)^2
+        }
+        else
+        {
+          t1kt <- (cumy1[t+1]-cumy1[k])/(t-k+1)
+          t2kt <- (cumy2[t+1]-cumy2[k])/(t-k+1)
+          t1kj <- (cumy1[j]-cumy1[k])/(j-k)
+          t2kj <- (cumy2[j]-cumy2[k])/(j-k)
+          D <- (t1kt - t1kj)^2 + (t2kt - t2kj)^2
+        }
+        info$m[i] <- eval_q_min(k, t+1) + (t-k+1)*(sqrt(D)-sqrt(R))^2
+      }
+    }
+    ######### 2 ######### adding new 3-points with t
+    i <- i + 1
+    info[i, ] <- c(t, t, eval_q_min(t, t+1))
+    for(k in indexSet)
+    {
+      i <- i + 1
+      t1k <- (cumy1[t+1]-cumy1[k])/(t-k+1)
+      t2k <- (cumy2[t+1]-cumy2[k])/(t-k+1)
+      info[i, ] <- c(t, k, eval_q(k, t, t1k, t2k))
+    }
+    indexSet <- c(indexSet, t)
+
+
+    ######
+    ###### STEP find the argmin, the last change-point
+    ######
+    nb[t] <- length(indexSet) ### count number of elements for DP
+    nrows[t] <- nrow(info)
+    min_temp <- Inf
+    for(k in indexSet)
+    {
+      eval <- eval_q_min(k, t+1)
+      if(eval < min_temp){min_temp <- eval; index <- k}
+    }
+    costQ[t+1] <- min_temp
+    cp[t] <- index - 1
+  }
+
+  ######
+  ###### backtracking step
+  ######
+  changepoints <- n # vector of change-point to build
+  current <- n
+
+  while(current > 1)
+  {
+    pointval <- cp[current] #new last change
+    changepoints <- c(pointval, changepoints) # update vector
+    current <- pointval
+  }
+  return(list(changepoints = changepoints[-1], nb = nb, cp = cp, costQ = costQ, nrows = nrows))
 }
+
 
 
--- simulations/test_OP_2D.R
+++ simulations/test_OP_2D.R
@@ -4,8 +4,8 @@ n <- 1000
 myBeta <- 10*log(n)
 v12 <- 1
 data <- dataGenerator2D(chpts <- 1:n,
-                        means1 = rnorm(1000, sd = 3),
-                        means2 = rnorm(1000, sd = 2),
+                        means1 = rnorm(1000, sd = 1),
+                        means2 = rnorm(1000, sd = 1),
                         sdNoise1 = v12,
                         sdNoise2 = v12)
 R1 <- OP_2D(data,beta =myBeta)
@@ -19,3 +19,38 @@ all(R1$changepoints == R2$changepoints)
 R2$changepoints
 plot(R2$nb, type = 'l')
 abline(v = R2$changepoints, col = 2)
+
+
+####
+
+
+
+n <- 1000
+myBeta <- 20*log(n)
+v12 <- 1
+data <- dataGenerator2D(chpts = n,
+                        means1 = 0,
+                        means2 = 0,
+                        sdNoise1 = 2,
+                        sdNoise2 = 1)
+
+
+
+R1 <- OP_2D_PELT(data = data, beta = myBeta)
+all(R1$changepoints == R2$changepoints)
+
+R2 <- OP_2D_1C(data = data, beta = myBeta)
+R2$nb
+R2$nrows
+
+ymax <- max(R1$nb, R2$nb)
+plot(R1$nb, type = 'l', ylim = c(0,ymax))
+par(new = TRUE)
+plot(R2$nb, type = 'l', col = 2, ylim = c(0,ymax))
+
+
+plot(R2$nb-R1$nb, type = 'l', col = 2)
+
+
+
+
