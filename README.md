9c2f3f896344e0703bf414bb79548f3e20014298
Vincent Runge <vrunge@tora.maths.univ-evry.fr>
2022-08-22 17:00:31 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# opac
+<a id="top"></a>
+
+# opac Vignette
+### Vincent Runge
+#### LaMME, Evry University
+### August 22, 2022
+
+> [Quick Start](#qs)
+
+## Quick Start
+
+The `opac` package can be installed from Github as follows:
+
+```r
+#devtools::install_github("vrunge/opac")
+library(opac)
+```
+
+[Back to Top](#top)
+
--- simulations/test_OP_2D.R
+++ simulations/test_OP_2D.R
@@ -1,13 +1,14 @@
 
 
 n <- 1000
-myBeta <- 10*log(n)
+myBeta <- 4*log(n)
 v12 <- 1
 data <- dataGenerator2D(chpts <- 1:n,
                         means1 = rnorm(1000, sd = 1),
                         means2 = rnorm(1000, sd = 1),
                         sdNoise1 = v12,
                         sdNoise2 = v12)
+plot(data$y2)
 R1 <- OP_2D(data,beta =myBeta)
 R1$changepoints
 R2 <- OP_2D_PELT(data,beta =myBeta)
@@ -18,6 +19,7 @@ all(R1$changepoints == R2$changepoints)
 ### plot ###
 R2$changepoints
 plot(R2$nb, type = 'l')
+
 abline(v = R2$changepoints, col = 2)
 
 
@@ -25,7 +27,7 @@ abline(v = R2$changepoints, col = 2)
 
 
 
-n <- 100
+n <- 20000
 myBeta <- 4*log(n)
 v12 <- 1
 data <- dataGenerator2D(chpts = c(n/2,n),
@@ -33,19 +35,20 @@ data <- dataGenerator2D(chpts = c(n/2,n),
                         means2 = c(0,1),
                         sdNoise1 = 1,
                         sdNoise2 = 1)
+plot(data$y2)
 
-R1 <- OP_2D_PELT(data = data, beta = myBeta)
-R2 <- OP_2D_1C(data = data, beta = myBeta)
+system.time(R1 <- OP_2D_PELT(data = data, beta = myBeta))
+system.time(R2 <- OP_2D_1C(data = data, beta = myBeta))
 all(R1$changepoints == R2$changepoints)
 
 
 
 which(R2$nb-R1$nb>0)
 
-ymax <- max(R1$nb, R2$nb)
-plot(R1$nb, type = 'l', ylim = c(0,ymax))
+ymax <- log(max(R1$nb, R2$nb))
+plot(log(R1$nb), type = 'l', ylim = c(0,ymax))
 par(new = TRUE)
-plot(R2$nb, type = 'l', col = 2, ylim = c(0,ymax))
+plot(log(R2$nb), type = 'l', col = 2, ylim = c(0,ymax))
 R0 <- OP_2D(data,beta =myBeta)
 
 R0$changepoints
